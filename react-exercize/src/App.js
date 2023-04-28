b9d64e9230b6a72fc0e1184ae84d4aadfc835d9f
sofyeahh <sofia.barboni@edu.unife.it>
2023-04-28 12:35:04 +0200

    feature: adds navbar + menu + mission

---
+++ react-exercize/src/App.js
@@ -0,0 +1,24 @@
+//styles
+import './App.css'
+
+// Components
+import Navbar from './components/Navbar'
+import Menu from './components/Menu'
+import Slider from './components/Slider'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className='container'>
+        <Menu/>
+        <Slider/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
